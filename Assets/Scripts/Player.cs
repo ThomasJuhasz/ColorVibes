34735aa572a206e033e6c43340559bf031af22d4
ThomasJuhasz <juhasz.thomas.jozsef@gmail.com>
2019-12-09 20:49:23 +0100

    Player Script Rotation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var eulerAngle = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(joystick.Vertical, joystick.Horizontal) * 180 / Mathf.PI));
+        transform.rotation = eulerAngle;
+    }
+}
